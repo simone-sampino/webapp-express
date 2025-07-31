28bc015cca77076475a4e3b59868ed8a9098e2d8
simone-sampino <s.samp98@gmail.com>
2025-07-31 21:26:07 +0200

    Refactor code structure for improved readability and maintainability

--- backend-api/controllers/movieController.js
+++ backend-api/controllers/movieController.js
@@ -1,9 +1,54 @@
+const connection = require("../database/connection");
+
 function index(req, res) {
-  res.send("show all movies inside the db");
+  const sql = "SELECT * FROM movies";
+
+  connection.query(sql, (err, result) => {
+    if (err)
+      return res.status(500).json({
+        error: true,
+        message: err.message,
+      });
+    res.json(result);
+  });
 }
 
 function show(req, res) {
-  res.send(`show the single movie ${req.params.id}`);
+  const { id } = req.params;
+
+  const sql = "SELECT * FROM movies WHERE id=?";
+
+  connection.execute(sql, [id], (err, result) => {
+    if (err)
+      return res.status(500).json({
+        error: true,
+        message: err.message,
+      });
+
+    if (result.lenght === 0) {
+      return res.status(404).json({
+        error: true,
+        message: "Not found",
+      });
+    }
+
+    const movie = result[0];
+
+    const reviewSql = "SELECT * FROM reviews WHERE movie_id=?";
+
+    connection.execute(reviewSql, [id], (err, result) => {
+      if (err)
+        return res.status(500).json({
+          error: true,
+          message: err.message,
+        });
+
+      const movieReviews = result;
+      movie.reviews = movieReviews;
+
+      res.json(movie);
+    });
+  });
 }
 
 module.exports = { index, show };
--- backend-api/server.js
+++ backend-api/server.js
@@ -3,10 +3,11 @@ const app = express();
 const PORT = process.env.PORT;
 
 const movieRouter = require("./routes/movies");
-const connection = require("./database/connection");
 
-app.use("/api/movies", movieRouter);
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log(`Server is listening http://127.0.0.1:${PORT}`);
 });
+
+app.use("/api/movies", movieRouter);
